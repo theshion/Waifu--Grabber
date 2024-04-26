ed6e7b0b4ddda5748fb7b75a3b8850f5732a3bc4
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-04-27 01:49:05 +0530

    Update waifu.py

--- waifu.py
+++ waifu.py
@@ -2,10 +2,10 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # Replace 'API_ID' and 'API_HASH' with your actual API credentials
-API_ID = "xxxxxx" 
-API_HASH = 'xxxxxxx'
-SESSION_STRING = "xxxxxxxx" # Your String Session Here 
-CLIENT_NAME = "xxxx"  # Any Random Name
+API_ID = "25071254" 
+API_HASH = '5cb7bac198160c6dcf76f11da7b26a45'
+SESSION_STRING = "BQF-jpYAAV5gjpV7tKgskfviPIgijHtol_5LbBuK7IWVRNkSILDTPR-I_zJsmATIP94duKNrT08AGkNFkT2ADnX16td9ND0ELKOZu7TUZzjV6VXYDJYIJHU9soS4_Ep_zmLthuBs_yTQH2rfe6F71O09VQ33QG45bCaGLqjVyplnmRr-2kY6dTMeGv1ejw_uiahPFh1beyK1BLFpQh7joxyEow8sXU3iivUEtX6ApCYHTyHu1-71AUV1aYNOELr0-hnMfRpDN4W0xNGCcXiL_cKojnW_j50sKz4B8eaw_yTdQp6-L_IqqiGHOlXZKkvOzHAhYwMTjDyv8FQIii2XDuR4L5DymwAAAAGPazsjAA" # Your String Session Here 
+CLIENT_NAME = "shion"  # Any Random Name
 
 # Create a Pyrogram client with the provided session string
 app = Client(session_string=SESSION_STRING, api_id=API_ID, api_hash=API_HASH, name=CLIENT_NAME)
